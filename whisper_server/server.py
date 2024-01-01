5788e6f8caefe4c3d7160e5a52b4e35431d26b80
N <47500890+avi-cenna@users.noreply.github.com>
2024-01-01 09:44:09 -0500

    update server to accept stop message

--- whisper_server/server.py
+++ whisper_server/server.py
@@ -1,4 +1,5 @@
 import os
+import queue
 import tempfile
 import threading
 import wave
@@ -10,12 +11,16 @@ import sounddevice as sd
 import webrtcvad
 import zmq
 
-from whisper_server.config import WhisperServerConfig, load_config
 from faster_whisper import WhisperModel
 from loguru import logger
 
+from whisper_server.config import WhisperServerConfig, load_config
+
+_stop_recording_event = threading.Event()
+
 
 def record(config: WhisperServerConfig) -> Path:
+    """Record an audio file from system input and return the path to the file."""
     sample_rate = 16000
     frame_duration = 30  # 30ms, supported values: 10, 20, 30
     buffer_duration = 300  # 300ms
@@ -35,16 +40,17 @@ def record(config: WhisperServerConfig) -> Path:
         blocksize=sample_rate * frame_duration // 1000,
         callback=lambda indata, frames, time, status: buffer.extend(indata[:, 0]),
     ):
-        while True:
+        while True and not _stop_recording_event.is_set():
             # logger.debug("Continuing recording")
             if len(buffer) < sample_rate * frame_duration // 1000:
                 # logger.debug("Buffer not full")
                 continue
 
             frame = buffer[: sample_rate * frame_duration // 1000]
-            buffer = buffer[sample_rate * frame_duration // 1000 :]
+            buffer = buffer[sample_rate * frame_duration // 1000:]
 
-            is_speech = vad.is_speech(np.array(frame).tobytes(), sample_rate)
+            # is_speech = vad.is_speech(np.array(frame).tobytes(), sample_rate)
+            is_speech = vad.is_speech(np.array(frame).tobytes(), sample_rate) or True
             if is_speech:
                 logger.debug("Speech detected")
                 recording.extend(frame)
@@ -69,7 +75,8 @@ def record(config: WhisperServerConfig) -> Path:
         return Path(temp_audio_file.name)
 
 
-def transcribe(wavfile: Path, config: WhisperServerConfig):
+def transcribe(wavfile: Path, config: WhisperServerConfig) -> str:
+    """Transcribe an audio file and return the transcription."""
     model_cfg = config.whisper_model_config
     model = WhisperModel(
         model_cfg.whisper_model_size,
@@ -85,11 +92,18 @@ def transcribe(wavfile: Path, config: WhisperServerConfig):
         initial_prompt=transciption_cfg.initial_prompt,
     )
     logger.debug(
-        "Detected language '%s' with probability %f"
-        % (info.language, info.language_probability)
+        "Detected language '%s' with probability %f" % (info.language, info.language_probability)
     )
     logger.debug("Finished transcription")
     return "".join(s.text for s in segments)
+
+
+def thread_record(config: WhisperServerConfig, result_queue):
+    try:
+        result = record(config)
+        result_queue.put(result)
+    except Exception as e:
+        result_queue.put(e)
 
 
 def main():
@@ -101,6 +115,9 @@ def main():
     cfg = load_config()
     logger.debug(cfg)
 
+    record_thread = None
+    result_queue = queue.Queue()
+
     while True:
         logger.debug(f"Waiting for message")
         message = socket.recv().decode("utf-8")
@@ -108,17 +125,30 @@ def main():
 
         match message:
             case "start":
-                wavfile = record(cfg)
-                tx = transcribe(wavfile, cfg)
-                logger.info(tx)
-                socket.send_string(tx)
+                _stop_recording_event.clear()
+                record_thread = threading.Thread(target=thread_record, args=(cfg, result_queue))
+                record_thread.start()
+                logger.debug("Started recording")
+                socket.send_string("Started recording")
+
+            case "stop":
+                logger.debug(f"Stopping recording")
+                _stop_recording_event.set()
+                record_thread.join()
+
+                wavfile = result_queue.get()
+                if isinstance(wavfile, Exception):
+                    raise wavfile
+                logger.debug(f"Recorded audio is saved at: {wavfile}")
+
+                # wavfile = record(cfg)
+                transcription = transcribe(wavfile, cfg)
+                logger.info(transcription)
+                socket.send_string(transcription)
             # case "get":
             #     socket.send_string(tx)
-            # TODO: add support for stop here
             case _:
-                # tx = transcribe(wavfile)
-                # socket.send_string(f"Received invalid message: {message}")
-                pass
+                socket.send_string(f"Received unsupported message: {message}")
 
 
 if __name__ == "__main__":
