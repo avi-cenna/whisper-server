003be07755884a1481db445db048b96e09f44987
N <47500890+avi-cenna@users.noreply.github.com>
2025-01-16 16:14:04 -0500

    autocorrect

--- src/whisper_server/autocorrect.py
+++ src/whisper_server/autocorrect.py
@@ -1,10 +1,40 @@
+import re
+
+from pydantic import BaseModel
+
 _capitalize = [
     'case management',
     'document management',
 ]
 
+from .config import find_poetry_project_root
+from loguru import logger
+
 
 def autocorrect(transcript: str):
     for cap in _capitalize:
         transcript = transcript.replace(cap, cap.title())
+    autos = parse_autocorrect_file()
+    for a in autos:
+        transcript = re.sub(r'\b' + a.original + r'\b', a.replacement, transcript, flags=re.I)
     return transcript
+
+
+class Autocorrection(BaseModel):
+    original: str
+    replacement: str
+
+
+def parse_autocorrect_file() -> list[Autocorrection]:
+    infile = find_poetry_project_root() / '.autocorrect'
+    if not infile.exists():
+        logger.warning(f'No autocorrect file found at {infile}')
+        return []
+
+    autocorrections = []
+    for line in infile.read_text().splitlines():
+        if not line.strip():
+            continue
+        original, replacement = line.split('->')
+        autocorrections.append(Autocorrection(original=original.strip(), replacement=replacement.strip()))
+    return autocorrections
