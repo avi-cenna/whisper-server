9272d27f12d595a8317676f0808761fcf2b00008
N <47500890+avi-cenna@users.noreply.github.com>
2025-01-17 20:43:28 -0500

    chore: Comment out faster_whisper import and add TODO for model change

--- src/whisper_server/record_transcribe.py
+++ src/whisper_server/record_transcribe.py
@@ -13,10 +13,26 @@ import openai
 import sounddevice as sd
 import webrtcvad
 
-from faster_whisper import WhisperModel
+# from faster_whisper import WhisperModel
 from loguru import logger
 
 from src.whisper_server.config import WhisperServerConfig
+
+# TODO: #12 instead of faster whisper i want to use whisper mlx. See example below
+
+# and i want to try this model
+# mlx-community/whisper-large-v3-turbo-q4
+
+# from whisperplus.pipelines import mlx_whisper
+# from whisperplus import download_youtube_to_mp3
+#
+# url = "https://www.youtube.com/watch?v=1__CAdTJ5JU"
+# audio_path = download_youtube_to_mp3(url)
+#
+# text = mlx_whisper.transcribe(
+#     audio_path, path_or_hf_repo="mlx-community/whisper-large-v3-mlx"
+# )["text"]
+# print(text)
 
 
 static_model = WhisperModel(
